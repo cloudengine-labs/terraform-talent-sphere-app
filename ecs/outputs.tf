06b3c14277ffd78cdefc9668bea2d5eaa6e33cb3
Saravanan Gnanaguru <g.gsaravanan@gmail.com>
2025-02-21 00:24:40 +0530

    added updated output

--- ecs/outputs.tf
+++ ecs/outputs.tf
@@ -7,3 +7,13 @@ output "backend_service_name" {
   description = "The name of the backend ECS service"
   value       = aws_ecs_service.backend.name
 }
+
+output "frontend_task_definition_arn" {
+  description = "The ARN of the frontend ECS task definition"
+  value       = aws_ecs_task_definition.frontend.arn
+}
+
+output "backend_task_definition_arn" {
+  description = "The ARN of the backend ECS task definition"
+  value       = aws_ecs_task_definition.backend.arn
+}
--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,14 @@ output "backend_service_name" {
   description = "The name of the backend ECS service"
   value       = module.ecs.backend_service_name
 }
+
+output "frontend_task_definition_arn" {
+  description = "The ARN of the frontend ECS task definition"
+  value       = module.ecs.frontend_task_definition_arn
+}
+
+output "backend_task_definition_arn" {
+  description = "The ARN of the backend ECS task definition"
+  value       = module.ecs.backend_task_definition_arn
+}
+
