bdab55c63a777b16ae797ccf39673f132dfd49ac
Saravanan Gnanaguru <g.gsaravanan@gmail.com>
2025-02-20 23:28:31 +0530

    updated ecs code

---
+++ ecs/main.tf
@@ -0,0 +1,106 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+}
+
+resource "aws_security_group" "ecs" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_cluster" "main" {
+  name = "talent-sphere-cluster"
+}
+
+resource "aws_ecs_task_definition" "frontend" {
+  family                   = "frontend-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name      = "frontend"
+      image     = "ghcr.io/${var.github_username}/frontend:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_task_definition" "backend" {
+  family                   = "backend-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name      = "backend"
+      image     = "ghcr.io/${var.github_username}/backend:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 5001
+          hostPort      = 5001
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "frontend" {
+  name            = "frontend-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.frontend.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = aws_subnet.public[*].id
+    security_groups = [aws_security_group.ecs.id]
+  }
+}
+
+resource "aws_ecs_service" "backend" {
+  name            = "backend-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.backend.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = aws_subnet.public[*].id
+    security_groups = [aws_security_group.ecs.id]
+  }
+}
--- ecs/outputs.tf
+++ ecs/outputs.tf
@@ -1,9 +1,9 @@
 output "frontend_service_name" {
   description = "The name of the frontend ECS service"
-  value       = module.ecs.frontend_service_name
+  value       = aws_ecs_service.frontend.name
 }
 
 output "backend_service_name" {
   description = "The name of the backend ECS service"
-  value       = module.ecs.backend_service_name
+  value       = aws_ecs_service.backend.name
 }
