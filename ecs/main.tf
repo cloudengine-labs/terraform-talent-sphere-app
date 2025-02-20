ce588e55f0e9273d67b226b1014d8d82d188cd3e
Saravanan Gnanaguru <g.gsaravanan@gmail.com>
2025-02-21 00:18:45 +0530

    updated module

--- ecs/main.tf
+++ ecs/main.tf
@@ -1,6 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
+
+data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
